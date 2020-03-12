345e4b39297e0de2972af96634a59497e2ed0217
Andres Long <andreslong95@gmail.com>
2020-03-11 22:07:14 -0500

    much functionality added

--- config/orm.js
+++ config/orm.js
@@ -5,12 +5,12 @@ const connection = require("./connection.js");
 const orm = {
     viewAll: () => {
         let query = `
-SELECT employee.id, first_name, last_name, roles.title, roles.salary, dep_name, manager_id
-FROM employee
-LEFT JOIN roles
-ON role_id = roles.id
-LEFT JOIN department
-ON department_id = department.id`
+        SELECT employee.id, first_name, last_name, roles.title, roles.salary, dep_name
+        FROM employee
+        LEFT JOIN roles
+        ON role_id = roles.id
+        LEFT JOIN department
+        ON department_id = department.id`
         connection.query(query, (err, result) =>{
             if (err) throw err;
             console.log("\n");
@@ -18,8 +18,106 @@ ON department_id = department.id`
             //
             console.log("\n\n\n\n\n\n\n\n\n");
             // functionality.begin();
+        });
+    },
+    viewOrdered: (val) => {
+        let query = `
+        SELECT employee.id, first_name, last_name, roles.title, roles.salary, dep_name
+        FROM employee
+        LEFT JOIN roles
+        ON role_id = roles.id
+        LEFT JOIN department
+        ON department_id = department.id
+        ORDER BY ?? DESC`
+        connection.query(query,[val], (err, result) =>{
+            if (err) throw err;
+            console.log("\n");
+            console.table(result);
+            //
+            console.log("\n\n\n\n\n\n\n\n\n");
+            // functionality.begin();
+        });
+    },
+    addEmployee: (first, last, title) => {
+        let roleID;
+        switch (title){
+            case "Lead Programmer": 
+                roleID = 1;
+                break;
+            case "Sales Lead":
+                roleID = 2;
+                break;
+            case "Hiring Manager":
+                roleID = 3;
+                break;
+            case "Accountant":
+                roleID = 4;
+                break;
+            case "Marketing Director":
+                roleID = 5;
+                break;
+            default:
+                break;
+        }
+        let queryOne = `
+        INSERT INTO employee (first_name, last_name, role_id)
+        VALUES (?,?,?);`
+        connection.query(queryOne,[first, last, roleID], (err, result) =>{
+            if (err) throw err;
+            console.log("\nAdded New Employee");
+            // console.log(result)
+            // functionality.begin();
+        });
+    },
+    remEmployee: (id) =>{
+        query =`
+        DELETE FROM employee WHERE employee.id = ?`
+        connection.query(query,[id], (err,res) =>{
+            if(err) throw err;
+            console.log("\nDeleted.");
+        })
+    },
+    updateRole: (employee, title) => {
+        //The proper thing to do here is set a sort of filter within the connection so as for it 
+        //to update live, but I wanted to see functionality first
+        let roleID;
+        switch (title){
+            case "Lead Programmer": 
+                roleID = 1;
+                break;
+            case "Sales Lead":
+                roleID = 2;
+                break;
+            case "Hiring Manager":
+                roleID = 3;
+                break;
+            case "Accountant":
+                roleID = 4;
+                break;
+            case "Marketing Director":
+                roleID = 5;
+                break;
+            default:
+                break;
+        }
+        query = `
+        UPDATE employee SET role_id = ? WHERE employee.id = ?`
+        connection.query(query, [roleID, employee], (err, result)=>{
+            if(err)throw err;
+            console.log("\nUpdated");
+        })
+    },
+    addDepartments: (depName) => {
+        query = `
+        INSERT INTO department (dep_name)
+        VALUES (?)`
+        connection.query(query, [depName], (err, result) => {
+            if (err) throw err;
+            console.log("\nDepartment Added.");
         })
     }
 }
+
+
 
 module.exports = orm;
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const inquirer = require("inquirer");
 const orm = require("./config/orm.js");
 
+//This is solely used for inquirer prompts
+let basicTitles= ["Lead Programmer", "Sales Lead", "Hiring Manager", "Accountant", "Marketing Director"];
+let basicDepartment = ["Engineering", "Sales", "Human Resources", "Accounting", "Marketing"];
+
 console.log(`
-_______________________________________________________________________
-|       (_      (_      (_      (_      (_      (_      (_      (_      |
-|        _)      _)      _)      _)      _)      _)      _)      _)     |
-|  _   _(  _   _(  _   _(  _   _(  _   _(  _   _(  _   _(  _   _(  _   _|
-|_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |
+________________________________________________________________________
 |      _)      _)      _)      _)      _)      _)      _)      _)       |
 |     (_      (_      (_      (_      (_      (_      (_      (_        |
 |_   _  )_   _  )_   _  )_   _  )_   _  )_   _  )_   _  )_   _  )_   _  |
@@ -17,10 +17,6 @@ _______________________________________________________________________
 |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |
 |      _)      _)      _)      _)      _)      _)      _)      _)       |
 |     (_      (_      (_      (_      (_      (_      (_      (_        |
-|_   _  )_   _  )_   _  )_   _  )_   _  )_   _  )_   _  )_   _  )_   _  |
-| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_|
-|       (_      (_      (_      (_      (_      (_      (_      (_      |
-|        _)      _)      _)      _)      _)      _)      _)      _)     |
 |_______(_______(_______(_______(_______(_______(_______(_______(_______|`)
 
 const execute = {
@@ -31,12 +27,14 @@ const execute = {
             message: "What do you want to do?",
             choices: [
             "View All Employees",
-            "View All Employess by Department",
-            "View All Employees by Manager",
+            "View All Employees by Department",
+            "View All Employees by Salary",
             "Add Employee",
             "Remove Employee",
             "Update Employee Role",
-            "Update Employee Manager"
+            "Add Department",
+            "Add Role",
+            "Exit"
         ],
         name: "choice"
         }
@@ -44,6 +42,90 @@ const execute = {
             if (data.choice === "View All Employees"){
                 orm.viewAll();
                 execute.begin();
+            }
+            else if (data.choice === "View All Employees by Department"){
+                orm.viewOrdered("department.id");
+                execute.begin();
+            }
+            else if(data.choice === "View All Employees by Salary"){
+                orm.viewOrdered("roles.salary");
+                execute.begin();
+            }
+            else if(data.choice === "Add Employee"){
+                inquirer.prompt([
+                    {
+                        message: "What is the new Employee's first name?",
+                        name: "name"
+                    },
+                    {
+                        message: "What is the new Employee's last name?",
+                        name: "last"
+                    },
+                    {
+                        type: "list",
+                        message: "Which role is the new Employee filling in?",
+                        choices: basicTitles,
+                        name: "title"
+                    }
+                ]).then((extData)=> {
+                    orm.addEmployee(extData.name, extData.last, extData.title, extData.salary)
+                    execute.begin();
+                });
+            }
+            else if(data.choice === "Remove Employee"){
+                inquirer.prompt([
+                    {
+                        message: "What Employee ID would you like to remove?",
+                        name: "employee"
+                    }
+                ]).then((remData) => {
+                    orm.remEmployee(remData.employee)
+                    execute.begin();
+                })
+            }
+            else if(data.choice === "Update Employee Role"){
+                inquirer.prompt([
+                    {
+                        message: "What Employee ID would you like to Update?",
+                        name: "updateRole"
+                    },
+                    {
+                        type: "list",
+                        message: "What new role is this person fulfilling?",
+                        choices: basicTitles,
+                        name: "newRole"
+                    }
+                ]).then((roleData) => {
+                    orm.updateRole(roleData.updateRole, roleData.newRole);
+                    execute.begin();
+                })
+            }
+            else if(data.choice === "Add Department"){
+                inquirer.prompt([
+                    {
+                        message: "Name your new Department",
+                        name: "newDepartment"
+                    }
+                ]).then((manageData) => {
+                    basicDepartment.push(manageData.newDepartment);
+                    orm.addDepartments(manageData.newDepartment);
+                    execute.begin();
+                })
+            }
+            else if(data.choice === "Add Role"){
+                inquirer.prompt([
+                    {
+                        message: "Name your new Role",
+                        name: "newRole"
+                    }
+                ]).then((manageData) => {
+                    basicRole.push(manageData.newRole);
+                    orm.addRoles(manageData.newRole);
+                    execute.begin();
+                })
+            }
+            else if(data.choice === "Exit"){
+                return;
             }
         })
     },
